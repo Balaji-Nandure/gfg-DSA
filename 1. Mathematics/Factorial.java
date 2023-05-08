478f5efa73524735d17d597e091903d1d4b04578
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-08 19:23:55 +0530

    factorialRecursive

--- 1. Mathematics/Factorial.java
+++ 1. Mathematics/Factorial.java
@@ -7,7 +7,17 @@ public class Factorial {
         return res;
     }
 
+    public static int factorialRecursive(int n) {
+        // base case
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorialRecursive(n - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        // System.out.println(factorial(5));
+        System.out.println(factorialRecursive(5));
     }
 }
