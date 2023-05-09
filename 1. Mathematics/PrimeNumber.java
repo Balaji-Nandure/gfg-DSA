25273fddbf695dd387aafc4d812d21a2ab1fb0ec
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-09 08:41:41 +0530

    Is prime

---
+++ 1. Mathematics/PrimeNumber.java
@@ -0,0 +1,58 @@
+public class PrimeNumber {
+    // Naive
+    // tc = O(n)
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Pro (check from 2 to <= sqrt(n) )
+    // tc = o(sqrt(n))
+    public static boolean isPrime1(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // more optimised
+    // adding two checks and looping from i=5 and step is i=i+6
+    public static boolean isPrime2(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        if (n == 2 || n == 3) {
+            return true;
+        }
+
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(11));
+        System.out.println(isPrime1(97));
+        System.out.println(isPrime2(99));
+    }
+}
